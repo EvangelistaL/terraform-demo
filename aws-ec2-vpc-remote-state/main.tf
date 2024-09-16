84545162b46f5b5f8007d42990efc558ff2b97d3
EvangelistaL <lucas.silva@sptech.school>
2024-09-16 18:23:26 -0300

    feature: ec2 creation

---
+++ aws-ec2-vpc-remote-state/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.67.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "hknl-remote-state-bucket"
+    key    = "aws-ec2/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  default_tags {
+    tags = local.common_tags
+  }
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "hknl-remote-state-bucket"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
